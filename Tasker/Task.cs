cf93d06ad0de3e7e00dd9764860489ef54fc2d3d
Harsimran <harsimran30@gmail.com>
2020-02-18 13:37:49 +1000

    Module 6 completed

--- Tasker/Task.cs
+++ Tasker/Task.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tasker
 {
     public class Task
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
+        [Required]
+        [Range(1, 5)]
         public int Priority { get; set; }
     }
 }
